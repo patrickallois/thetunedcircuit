c246f180ff6c27a29bd0c4cce29ddc888e39d367
Patrick Allois <patrickallois@gmail.com>
2024-06-23 13:01:35 -0400

    feat: add ttc domain buckets

--- lib/thetunedcircuit-stack.ts
+++ lib/thetunedcircuit-stack.ts
@@ -4,5 +4,17 @@ import { Construct } from "constructs";
 export class ThetunedcircuitStack extends cdk.Stack {
 	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
 		super(scope, id, props);
+
+		const subDomain = "www.thetunedcircuit.com";
+		const rootDomain = "thetunedcircuit.com";
+
+		new cdk.aws_s3.Bucket(this, subDomain, {
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			bucketName: subDomain,
+		});
+		new cdk.aws_s3.Bucket(this, rootDomain, {
+			removalPolicy: cdk.RemovalPolicy.DESTROY,
+			bucketName: rootDomain,
+		});
 	}
 }
